b87a9299fc7de8aea764742fbd4645df5e9d8cef
mwagg <michael@guerillatactics.co.uk>
2016-01-07 17:11:02 +0000

    Using CSV format for COPY command

--- lib/postgres_to_redshift.rb
+++ lib/postgres_to_redshift.rb
@@ -97,7 +97,7 @@ class PostgresToRedshift
     zip = Zlib::GzipWriter.new(buffer)
 
     puts "Downloading #{table}"
-    copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{table.name}) TO STDOUT WITH DELIMITER '|'"
+    copy_command = "COPY (SELECT #{table.columns_for_copy} FROM #{table.name}) TO STDOUT WITH CSV DELIMITER '|'"
 
     source_connection.copy_data(copy_command) do
       while row = source_connection.get_copy_data
@@ -125,7 +125,7 @@ class PostgresToRedshift
 
     target_connection.exec("CREATE TABLE #{PostgresToRedshift.target_schema}.#{table.target_table_name} (#{table.columns_for_create})")
 
-    target_connection.exec("COPY #{PostgresToRedshift.target_schema}.#{table.target_table_name} FROM 's3://#{ENV['S3_DATABASE_EXPORT_BUCKET']}/export/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' GZIP TRUNCATECOLUMNS ESCAPE DELIMITER as '|';")
+    target_connection.exec("COPY #{PostgresToRedshift.target_schema}.#{table.target_table_name} FROM 's3://#{ENV['S3_DATABASE_EXPORT_BUCKET']}/export/#{table.target_table_name}.psv.gz' CREDENTIALS 'aws_access_key_id=#{ENV['S3_DATABASE_EXPORT_ID']};aws_secret_access_key=#{ENV['S3_DATABASE_EXPORT_KEY']}' CSV GZIP TRUNCATECOLUMNS DELIMITER as '|';")
 
     target_connection.exec("COMMIT;")
   end
